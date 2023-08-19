be638a66bba83c302ceb66e2c4455a87d4239ba7
Nate Tedesco <Natetedesco@icloud.com>
2023-08-19 14:34:15 -0700

    Initial Commit

---
+++ Sunrise Alarm/Sunrise_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sunrise_AlarmApp.swift
+//  Sunrise Alarm
+//
+//  Created by Nate Tedesco on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Sunrise_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
